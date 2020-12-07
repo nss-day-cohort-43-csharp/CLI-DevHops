9015835ab989e9590e9bf4dd7242c35d74d48f4c
corneliajohnson <corneliaj91@gmail.com>
2020-12-07 11:52:11 -0500

    added jornal GetAll

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -8,5 +8,38 @@ namespace TabloidCLI
 {
     class JournalRepository : DatabaseConnector
     {
+        public JournalRepository(string connectionString) : base(connectionString) { }
+
+        public List<Journal> GetAll()
+        {
+            using(SqlConnection conn = Connection)
+            {
+                conn.Open();
+                {
+                    using(SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"SELECT Id, Title, Content, CreateDateTime
+                                            FROM Journal";
+                        List<Journal> journals = new List<Journal>();
+
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        
+                        while(reader.Read())
+                        {
+                            Journal journal = new Journal
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                                Title = reader.GetString(reader.GetOrdinal("Title")),
+                                Content = reader.GetString(reader.GetOrdinal("Content")),
+                                CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime"))
+                            };
+                            journals.Add(journal);
+                        }
+                        reader.Close();
+                        return journals;
+                    }
+                }
+            }
+        }
     }
 }
