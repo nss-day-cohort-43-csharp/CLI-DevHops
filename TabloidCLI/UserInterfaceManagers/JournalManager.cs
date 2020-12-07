5771862557f5120b61a8671b2d6a06610647d57b
corneliajohnson <corneliaj91@gmail.com>
2020-12-07 12:57:15 -0500

    added commits

--- TabloidCLI/UserInterfaceManagers/JournalManager.cs
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -6,10 +6,12 @@ namespace TabloidCLI.UserInterfaceManagers
 {
     class JournalManager : IUserInterfaceManager
     {
+        // declare fields
         private readonly IUserInterfaceManager _parentUI;
         private JournalRepository _journalRepository;
         private string _connectionString;
 
+        // initialize fields in the constructor
         public JournalManager(IUserInterfaceManager parentUI, string connectionString)
         {
             _parentUI = parentUI;
@@ -17,26 +19,26 @@ namespace TabloidCLI.UserInterfaceManagers
             _connectionString = connectionString;
         }
 
+        // excute code when returned
         public IUserInterfaceManager Execute()
         {
             Console.WriteLine("Journal Menu");
             Console.WriteLine(" 1) List Journals");
-            //Console.WriteLine(" 2) Journal Details");
-            //Console.WriteLine(" 3) Add Journal");
-            //Console.WriteLine(" 4) Edit Journal");
-           // Console.WriteLine(" 5) Remove Journal");
             Console.WriteLine(" 0) Go Back");
 
             Console.Write("> ");
+            //Read User's choice/selection
             string choice = Console.ReadLine();
 
             switch(choice)
             {
                 case "1":
-                    List();
+                    //Show all Jornal entries
+                    List(); 
                     return this;
 
                 case "0":
+                    //Return to preivous menu
                     return _parentUI;
                 default:
                     Console.WriteLine("Invalid Selection");
@@ -47,7 +49,7 @@ namespace TabloidCLI.UserInterfaceManagers
         private void List()
         {
             List<Journal> journals = _journalRepository.GetAll();
-            journals.ForEach(j => Console.WriteLine($"{j.Title} - {j.Content} - {j.CreateDateTime}"));
+            journals.ForEach(j => Console.WriteLine($"{j.Title}"));
             }
     }
 }
