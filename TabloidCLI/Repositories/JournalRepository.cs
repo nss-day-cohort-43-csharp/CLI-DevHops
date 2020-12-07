c9a5ed89cf462c8585c2193fcee9e97f8c17e92f
corneliajohnson <corneliaj91@gmail.com>
2020-12-07 11:24:12 -0500

    added jounal file

---
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    class JournalRepository : DatabaseConnector
+    {
+    }
+}
