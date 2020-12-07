3b4d23f90e52041bb352f48b1aa077a37eed3a92
corneliajohnson <corneliaj91@gmail.com>
2020-12-07 12:03:10 -0500

    save

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _journalRepository = new JournalRepository(connectionString);
+            _connectionString = connectionString;
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) List Journals");
+            Console.WriteLine(" 2) Journal Details");
+            Console.WriteLine(" 3) Add Journal");
+            Console.WriteLine(" 4) Edit Journal");
+            Console.WriteLine(" 5) Remove Journal");
+            Console.WriteLine(" 0) Go Back");
+        }
+    }
+}
