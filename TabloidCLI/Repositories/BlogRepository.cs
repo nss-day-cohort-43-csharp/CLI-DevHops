1f84384173ca0648fccd7586ad2595bc82b39de2
bloges56 <brady.drew.logan@gmail.com>
2020-12-07 11:46:47 -0600

    created a get all method that returns a list of all the blogs in the database

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -11,6 +11,55 @@ namespace TabloidCLI.Repositories
         /// repository constructor
         public BlogRepository(string _connectionString) : base(_connectionString) { }
 
+        // returns a list of all the blogs in the database
+        public List<Blog> GetAll()
+        {
+            // start a connection
+            using (SqlConnection conn = Connection)
+            {
+                // open the connection
+                conn.Open();
+
+                // use a command
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // create the sql command
+                    cmd.CommandText = @"SELECT Title, URL
+                                        From Blog";
+
+                    // execute reader and store the returned reader
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // instaniate an empty blog list to store the blogs in
+                    List<Blog> blogs = new List<Blog>() { };
+
+                    // read while there is still data
+                    while(reader.Read())
+                    {
+                        // get the values
+                        string title = reader.GetString(reader.GetOrdinal("Title"));
+                        string url = reader.GetString(reader.GetOrdinal("URL"));
+
+                        // create a new blog object
+                        Blog blog = new Blog()
+                        {
+                            Title = title,
+                            Url = url
+                        };
+
+                        // add the new blog to the list of blogs
+                        blogs.Add(blog);
+                    }
+
+                    //close the reader
+                    reader.Close();
+
+                    // return the list of blogs
+                    return blogs;
+                }
+            }
+        }
+
         /// adds a blog to the database
         public void Insert(Blog blog)
         {
