404436881ca69464742d668bb1a0f661ea55a94c
blaker814 <rab0196@yahoo.com>
2020-12-07 11:19:09 -0600

    Added fields for user input

--- TabloidCLI/UserInterfaceManagers/PostManager.cs
+++ TabloidCLI/UserInterfaceManagers/PostManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TabloidCLI.Models;
 using TabloidCLI.Repositories;
 
 namespace TabloidCLI.UserInterfaceManagers
@@ -35,7 +36,7 @@ namespace TabloidCLI.UserInterfaceManagers
                     
                     return this;
                 case "2":
-
+                    Add();
                     return this;
                 case "3":
                     
@@ -49,6 +50,22 @@ namespace TabloidCLI.UserInterfaceManagers
                     Console.WriteLine("Invalid Selection");
                     return this;
             }
+        }
+        private void Add()
+        {
+            Console.WriteLine("New Post");
+            Post post = new Post();
+
+            Console.Write("Title: ");
+            post.Title = Console.ReadLine();
+
+            Console.Write("URL: ");
+            post.Url = Console.ReadLine();
+
+            Console.Write("Publication date (YYYY-MM-DD): ");
+            post.PublishDateTime = Convert.ToDateTime(Console.ReadLine());
+
+            _postRepository.Insert(post);
         }
     }
 }
