17abe36752cc58b5ceded92b00c54e9c368f55e3
bloges56 <brady.drew.logan@gmail.com>
2020-12-07 10:24:15 -0600

    created blogrepository and gave it an insert method

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Models;
+using Microsoft.Data.SqlClient;
+
+namespace TabloidCLI.Repositories
+{
+    class BlogRepository : DatabaseConnector
+    {
+        /// repository constructor
+        public BlogRepository(string _connectionString) : base(_connectionString) { }
+
+        /// adds a blog to the database
+        public void Insert(Blog blog)
+        {
+            /// start a connection
+            using (SqlConnection conn = Connection)
+            {
+                /// open the connection
+                conn.Open();
+
+                /// use command
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Blog (Title, URL)
+                                        VALUES (@title, @url)";
+                    cmd.Parameters.AddWithValue("@ititle", blog.Title);
+                    cmd.Parameters.AddWithValue("@url", blog.Url);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
