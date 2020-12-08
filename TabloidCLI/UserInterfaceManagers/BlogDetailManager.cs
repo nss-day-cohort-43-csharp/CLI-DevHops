7f9a179c9ed1d7d10ce0acceb1bb5d4dd251cf88
bloges56 <brady.drew.logan@gmail.com>
2020-12-08 13:40:46 -0600

    created blog detail manager

---
+++ TabloidCLI/UserInterfaceManagers/BlogDetailManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TabloidCLI.Repositories;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    class BlogDetailManager : IUserInterfaceManager
+    {
+        // declare fields
+        private IUserInterfaceManager _parentUI;
+        private BlogRepository _blogRepo;
+
+
+        public BlogDetailManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _blogRepo = new BlogRepository(connectionString);
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+
+        }
+
+    }
+}
